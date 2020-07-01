1a6b558322299464975cc04c072acaeffbee2846
Frank Carr <gundamtrooperx@gmail.com>
2020-07-01 02:33:48 -0700

    update changes

--- quiz.js
+++ quiz.js
@@ -1,3 +1,8 @@
+// the list of functions below and if statements are something i looked up online to understand piece by piece before applting them into my code.
+// "this" is still confusing 
+// sets up the score and the questions showing the text, choices, and questions.
+// as well as letting the code know what the correct answer is when selected for the question
+
 function Quiz(questions) {
     this.score = 0;
     this.questions = questions;
@@ -31,6 +36,9 @@ Question.prototype.isCorrectAnswer = function(choice) {
     return this.answer === choice;
 }
  
+
+// shows the scores, questions, and options for the questions by creating if/else statements with a for loop to cycle 
+// through the choices for each question.
  
 function populate() {
     if(quiz.isEnded()) {
@@ -38,13 +46,13 @@ function populate() {
     }
     else {
         // show question
-        var element = document.getElementById("question");
+        const element = document.getElementById("question");
         element.innerHTML = quiz.getQuestionIndex().text;
  
         // show options
-        var choices = quiz.getQuestionIndex().choices;
-        for(var i = 0; i < choices.length; i++) {
-            var element = document.getElementById("choice" + i);
+        const choices = quiz.getQuestionIndex().choices;
+        for(let i = 0; i < choices.length; i++) {
+            const element = document.getElementById("choice" + i);
             element.innerHTML = choices[i];
             guess("btn" + i, choices[i]);
         }
@@ -54,29 +62,33 @@ function populate() {
 };
  
 function guess(id, guess) {
-    var button = document.getElementById(id);
+    const button = document.getElementById(id);
     button.onclick = function() {
         quiz.guess(guess);
         populate();
     }
 };
  
- 
+ // progression through the number of questions for the quiz with a function to help present that 
+ // same for showing the score
+
 function showProgress() {
-    var currentQuestionNumber = quiz.questionIndex + 1;
-    var element = document.getElementById("progress");
+    const currentQuestionNumber = quiz.questionIndex + 1;
+    const element = document.getElementById("progress");
     element.innerHTML = "Question " + currentQuestionNumber + " of " + quiz.questions.length;
 };
  
 function showScores() {
-    var gameOverHTML = "<h1>Result</h1>";
+    const gameOverHTML = "<h1>Result</h1>";
     gameOverHTML += "<h2 id='score'> Your scores: " + quiz.score + "</h2>";
-    var element = document.getElementById("quiz");
+    const element = document.getElementById("quiz");
     element.innerHTML = gameOverHTML;
 };
  
-// create questions here
-var questions = [
+// questions are listed here with the arrays for choices and the correct answer for those questions.
+// saw many variations to do this either through html and do it by spanning them
+// or listing the questions together with a variable and arrays
+const questions = [
     new Question("What does HTML stand for?", ["Hyper Team Monkey Life", "Holy Text Money Lore", "Horsey Tongue Markup Laundry", "Hyper Text Markup Language"], "Hyper Text Markup Language"),
     new Question("What does CSS stand for?", ["Cars Standing Still", "Computers Shall Serve", "Community Services School", "Cascading Style Sheets"], "Cascading Style Sheets"),
     new Question("Arrays in JavaScript can be used to store ____.", ["numbers and strings", "other arrays", "booleans", "all of the above"], "all of the above"),
@@ -85,10 +97,13 @@ var questions = [
 ];
  
 // create quiz
-var quiz = new Quiz(questions);
+const quiz = new Quiz(questions);
  
 // display quiz
 populate();
+
+// found this online hoping it would give me some sort of hints on tying my timer in with my list of choices then having it give my chosen wrpng answer be 
+// reduced with the -5 time
 
 (function() {
     var sec = 100;
